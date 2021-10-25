c2a0a9140728e72fb01b9cf75cda360f7afd05f9
Giorgos Dimitriou <giwrgosdi@gmail.com>
2021-10-25 17:07:43 +0100

    Separate pods info functionality to a separate package. Enchance the output with tabwritter

--- pkg/resources/pod.go
+++ pkg/resources/pod.go
@@ -3,8 +3,10 @@ package resources
 import (
 	"context"
 	"fmt"
+	"os"
+	"sort"
 
-	//"text/tabwritter"
+	"text/tabwriter"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -18,60 +20,68 @@ type Resp struct {
 	mlim string
 }
 
-type Aggregator struct {
-	creq []int64
-	clim []int64
-	mreq []int64
-	mlim []int64
+type Container struct {
+	name string
+	creq int64
+	clim int64
+	mreq int64
+	mlim int64
 }
 
 type P struct {
-	name string
-	Aggregator
+	name       string
+	containers []Container
+	creqSum    int64
+	climSum    int64
+	mreqSum    int64
+	mlimSum    int64
 }
 
-//func getWriter() tabwritter.Writter {
-//	w := new(tabwritter.Writter)
-//	w.Init(os.Stdout, 8, 8, 5, '\t', 0)
-//	return w
-//}
-//
+func getWriter() *tabwriter.Writer {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 8, 8, 5, '\t', 0)
+	return w
+}
 
-func sum(array []int64) int64 {
-	var result int64
-	for _, v := range array {
-		result += v
+func (pod P) sumResource() P {
+	for _, cntr := range pod.containers {
+		pod.creqSum += cntr.creq
+		pod.mreqSum += cntr.mreq
+		pod.climSum += cntr.clim
+		pod.mlimSum += cntr.mlim
 	}
-	return result
+	pod.mlimSum = pod.mlimSum / 1024 / 1024
+	pod.mreqSum = pod.mreqSum / 1024 / 1024
+	return pod
 }
 
 func GetPodInfo(ns, node string, client *kubernetes.Clientset) {
 	var listOfPods []P
 	var p P
-	//w := getWriter()
+	w := getWriter()
+	defer w.Flush()
 	pods, _ := client.CoreV1().Pods(ns).List(context.TODO(), metav1.ListOptions{})
 	fmt.Printf("Node : %s\n", node)
 	for _, pod := range pods.Items {
-		p = P{}
 		if pod.Spec.NodeName == node {
-			fmt.Printf("POD: %s\n", pod.ObjectMeta.Name)
+			//fmt.Printf("POD: %s\n", pod.ObjectMeta.Name)
 			p.name = pod.ObjectMeta.Name
-			for _, container := range pod.Spec.Containers {
-				fmt.Printf("Container: %s\n", container.Name)
-				cpureq := container.Resources.Requests.Cpu().MilliValue()
-				//memreq := container.Resources.Requests.Memory().ScaledValue(6)
-				////cpulim := container.Resources.Limits.Cpu().MilliValue()
-				//////memlim := container.Resources.Limits.Memory().ScaledValue(6)
-				p.creq = append(p.creq, cpureq)
-				////p.clim = append(p.creq, cpulim)
-				//p.mreq = append(p.creq, memreq)
-				//p.mlim = append(p.creq, memlim)
+			p.containers = make([]Container, len(pod.Spec.Containers))
+			for idx, container := range pod.Spec.Containers {
+				//fmt.Printf("Container: %s\n", container.Name)
+				p.containers[idx].name = container.Name
+				p.containers[idx].creq = container.Resources.Requests.Cpu().MilliValue()
+				p.containers[idx].mreq = container.Resources.Requests.Memory().Value()
+				p.containers[idx].clim = container.Resources.Limits.Cpu().MilliValue()
+				p.containers[idx].mlim = container.Resources.Limits.Memory().Value()
 			}
-			fmt.Println(p)
 			listOfPods = append(listOfPods, p)
 		}
 	}
-	fmt.Println(listOfPods)
-	//fmt.Fprintf(w, "\nPod\tCPU req/limit\tMem req/limit")
-
+	fmt.Fprintf(w, "\nPOD\tCPU REQUEST\tCPU LIMIT\tMEM REQUEST\tMEM LIMIT")
+	for _, pod := range listOfPods {
+		pod = pod.sumResource()
+		fmt.Fprintf(w, "\n%s\t%dm\t%dm\t%dMi\t%dMi", pod.name, pod.creqSum, pod.climSum, pod.mreqSum, pod.mlimSum)
+	}
+	fmt.Fprintf(w, "\n")
 }
