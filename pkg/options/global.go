e22f0f84215f09cb875ec9cb2878b3ff4883b41a
Giorgos Dimitriou <giwrgosdi@gmail.com>
2022-02-21 18:00:16 +0000

    make kubeconfig a default value and load it from an env var

--- pkg/options/global.go
+++ pkg/options/global.go
@@ -1,6 +1,9 @@
 package options
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
@@ -25,14 +28,24 @@ func NewNodeInfoOptions(streams genericclioptions.IOStreams) *NodeInfoOptions {
 
 func (o *NodeInfoOptions) Complete(c *cobra.Command, args []string) error {
 	var err error
+	homeDir, _ := os.UserHomeDir()
 	o.Namespace, err = c.Flags().GetString("namespace")
 	if err != nil {
 		return err
 	}
-	o.Kubeconfig, err = c.Flags().GetString("kubeconfig")
-	if err != nil {
-		return err
+	o.Kubeconfig = fmt.Sprintf("%s/.kube/config", homeDir)
+
+	if value, _ := c.Flags().GetString("kubeconfig"); value != "" {
+		o.Kubeconfig, err = c.Flags().GetString("kubeconfig")
+		if err != nil {
+			return err
+		}
 	}
+	if os.Getenv("KUBECONFIG") != "" {
+		o.Kubeconfig = os.Getenv("KUBECONFIG")
+	}
+	fmt.Println(o.Kubeconfig)
+
 	o.Args = args
 	return nil
 }
